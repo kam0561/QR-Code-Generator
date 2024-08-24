2f38835230a0222e8841964fed10704fd5e0a946
Kunwar <kunwarabhay05@gmail.com>
2024-08-24 22:23:30 +0530

    link gets saved as .txt file

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ inquirer
     const URL= answers.URL;
     var qr_svg = qr.image(URL);
     qr_svg.pipe(fs.createWriteStream('qr-image.png'));
+    fs.writeFile('URL.txt', URL, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
     // Use user feedback for... whatever!!
   })
   .catch((error) => {
